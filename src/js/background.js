c2435f2555a0ec1fadf1288bae8d1492d764e29f
Yoland Yan <yoland68@gmail.com>
2017-04-17 14:14:06 -0700

    update

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,11 @@
-var newUrl = "chrome-extension://laankejkbhbdhmipfmgcngdelahlfoji/options.html"
-setInterval(function doIt() {
+function doIt() {
+  var newUrl = "chrome-extension://laankejkbhbdhmipfmgcngdelahlfoji/options.html"
   chrome.tabs.create({ url: newUrl, active:false }, function(tab){
     chrome.tabs.remove(tab.id);
   });
-  return doIt;
-}(), 1000*60*10);
+}
+setInterval(doIt, 1000*60*10);
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+  doIt();
+})
+
